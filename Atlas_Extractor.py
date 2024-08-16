588b5bcdc2bf691a137390e4ee5fe3f612890252
Alireza Doustmohammadi <alireza.dostmohammadi@uni-due.de>
2024-08-17 00:34:30 +0200

    feature extraction

--- Atlas_Extractor.py
+++ Atlas_Extractor.py
@@ -1,79 +1,83 @@
 from nilearn import datasets
+import nilearn as nib
 from nilearn.datasets import fetch_abide_pcp
 from nilearn.input_data import NiftiMapsMasker
 from nilearn import image
+import nibabel
 from nilearn.connectome import ConnectivityMeasure
 import numpy as np
 from nilearn import plotting
 import glob
 import os
 
-def Save_To_File(filename,corrmatrix,out_dir):
+
+def Save_To_File(filename, corrmatrix, out_dir):
     basename = os.path.basename(filename)
-    newname = basename[0:len(basename)-7]
-    np.save("{}{}.npy".format(out_dir,newname),corrmatrix)
+    newname = basename[0:len(basename) - 7]
+    np.save("{}{}.npy".format(out_dir, newname), corrmatrix)
 
-fmri_path = "data/ABIDE_pcp/newdata/*.gz"
-out_dir = "Atlas_Extractor/NilearnNew/"
 
-atlas_dir = "Atlas/"
+if __name__ == '__main__':
 
-craddock = datasets.fetch_atlas_craddock_2012(atlas_dir)
-smith = datasets.fetch_atlas_smith_2009(atlas_dir)
-msdl = datasets.fetch_atlas_msdl(atlas_dir)
+    # path of FMRI files
+    fmri_path = "dat/Outputs/cpac/filt_global/func_preproc/*.gz"
+    atlas_dir = "Atlas/"
 
-hard = datasets.fetch_atlas_harvard_oxford('sub-prob-1mm',atlas_dir)
-#mutli = datasets.fetch_atlas_basc_multiscale_2015("sym", atlas_dir)
-difumo = datasets.fetch_atlas_difumo(dimension=512,resolution_mm=3,data_dir= atlas_dir)
+    out_dir = "Atlas_Extractor/NilearnNew/"
 
-#image = image.load_img("ROI/cc400_roi_atlas.nii/cc400.nii")
+    os.makedirs(out_dir, exist_ok=True)
 
-craddockmasker = NiftiMapsMasker(maps_img=craddock.scorr_mean, standardize=True, memory='nilearn_cache', verbose=0)
-smithmasker70 = NiftiMapsMasker(maps_img=smith.bm70, standardize=True, memory='nilearn_cache', verbose=0)
-smithmaskerr70 = NiftiMapsMasker(maps_img=smith.rsn70, standardize=True, memory='nilearn_cache', verbose=0)
-hardmasker = NiftiMapsMasker(maps_img=hard.maps, standardize=True, memory='nilearn_cache', verbose=0)
-difumomasker = NiftiMapsMasker(maps_img=difumo.maps, standardize=True, memory='nilearn_cache',memory_level=1, verbose=0)
-msdlmasker = NiftiMapsMasker(maps_img=msdl.maps, standardize=True, memory='nilearn_cache', verbose=0)
+    # Load atlases directly from local files
+    craddock = nibabel.load(os.path.join(atlas_dir, 'craddock_2012/scorr05_mean_all.nii.gz'))
+    smith_bm70 = nibabel.load(os.path.join(atlas_dir, 'smith_2009/bm70.nii.gz'))
+    smith_rsn70 = nibabel.load(os.path.join(atlas_dir, 'smith_2009/rsn70.nii.gz'))
+    msdl = nibabel.load(os.path.join(atlas_dir, 'msdl_atlas/MSDL_rois/msdl_rois.nii'))
+    hard = nibabel.load(os.path.join(atlas_dir, 'fsl/data/atlases/HarvardOxford/HarvardOxford-sub-prob-1mm.nii.gz'))
+    difumo = nibabel.load(os.path.join(atlas_dir, 'difumo_atlases/512/3mm/maps.nii.gz'))
 
-for file in glob.glob(fmri_path):
-    try:
-        print(file)
-        time_series = difumomasker.fit_transform(file)
-        print (time_series.shape)
-        correlation_measure = ConnectivityMeasure(kind='correlation')
-        correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-        Save_To_File(file, correlation_matrix, out_dir + "difumo512/")
-    except:
-        print("Error in {}".format(file))
+    # craddock = datasets.fetch_atlas_craddock_2012(atlas_dir)
+    # smith = datasets.fetch_atlas_smith_2009(atlas_dir)
+    # msdl = datasets.fetch_atlas_msdl(atlas_dir)
+    # hard = datasets.fetch_atlas_harvard_oxford('sub-prob-1mm', atlas_dir)
+    # mutli = datasets.fetch_atlas_basc_multiscale_2015("sym", atlas_dir)
+    # difumo = datasets.fetch_atlas_difumo(dimension=512, resolution_mm=3, data_dir=atlas_dir)
 
-    """time_series = hardmasker.fit_transform(file)
-    print (time_series.shape)
-    correlation_measure = ConnectivityMeasure(kind='correlation')
-    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-    Save_To_File(file, correlation_matrix, out_dir + "hard/")
+    # image = image.load_img("ROI/cc400_roi_atlas.nii/cc400.nii")
 
-    time_series = smithmaskerr70.fit_transform(file)
-    print (time_series.shape)
-    correlation_measure = ConnectivityMeasure(kind='correlation')
-    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-    Save_To_File(file, correlation_matrix, out_dir + "rsmith70/")
+    # craddockmasker = NiftiMapsMasker(maps_img=craddock.scorr_mean, standardize=True, memory='nilearn_cache', verbose=0)
+    # smithmasker70 = NiftiMapsMasker(maps_img=smith.bm70, standardize=True, memory='nilearn_cache', verbose=0)
+    # smithmaskerr70 = NiftiMapsMasker(maps_img=smith.rsn70, standardize=True, memory='nilearn_cache', verbose=0)
+    # msdlmasker = NiftiMapsMasker(maps_img=msdl.maps, standardize=True, memory='nilearn_cache', verbose=0)
 
-    time_series = msdlmasker.fit_transform(file)
-    print (time_series.shape)
-    correlation_measure = ConnectivityMeasure(kind='correlation')
-    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-    Save_To_File(file, correlation_matrix, out_dir + "msdl/")
+    # hardmasker = NiftiMapsMasker(maps_img=hard.maps, standardize=True, memory='nilearn_cache', verbose=0)
+    #difumomasker = NiftiMapsMasker(maps_img=difumo.maps, standardize=True, memory='nilearn_cache', memory_level=1,
+    #                               verbose=0)
 
-    time_series = smithmasker70.fit_transform(file)
-    print (time_series.shape)
-    correlation_measure = ConnectivityMeasure(kind='correlation')
-    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-    Save_To_File(file, correlation_matrix, out_dir + "smith70/")
+    # Create the NiftiMapsMasker objects
+    craddockmasker = NiftiMapsMasker(maps_img=craddock, standardize=True, memory='nilearn_cache', verbose=0)
+    smithmasker70 = NiftiMapsMasker(maps_img=smith_bm70, standardize=True, memory='nilearn_cache', verbose=0)
+    smithmaskerr70 = NiftiMapsMasker(maps_img=smith_rsn70, standardize=True, memory='nilearn_cache', verbose=0)
+    hardmasker = NiftiMapsMasker(maps_img=hard, standardize=True, memory='nilearn_cache', verbose=0)
+    difumomasker = NiftiMapsMasker(maps_img=difumo, standardize=True, memory='nilearn_cache', memory_level=1, verbose=0)
+    msdlmasker = NiftiMapsMasker(maps_img=msdl, standardize=True, memory='nilearn_cache', verbose=0)
 
-    time_series = craddockmasker.fit_transform(file)
-    print (time_series.shape)
-    correlation_measure = ConnectivityMeasure(kind='correlation')
-    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-    Save_To_File(file, correlation_matrix, out_dir + "craddock/")"""
+    atlas_dict = {'difumo512': difumomasker, 'hard': hardmasker, 'rsmith70': smithmaskerr70, 'msdl': msdlmasker,
+                  'smith70': smithmasker70, 'craddock': craddockmasker}
 
-#web site: https://joaoloula.github.io/functional-atlas.html
+    for atlas in atlas_dict.keys():
+        for file in glob.glob(fmri_path):
+            try:
+                print(file)
+                time_series = atlas_dict[atlas].fit_transform(file)
+                print(time_series.shape)
+                correlation_measure = ConnectivityMeasure(kind='correlation')
+                correlation_matrix = correlation_measure.fit_transform([time_series])[0]
+
+                os.makedirs(out_dir + atlas, exist_ok=True)
+
+                Save_To_File(file, correlation_matrix, out_dir + atlas + "/")
+
+            except:
+                print("Error in {}".format(file))
+
+    # web site: https://joaoloula.github.io/functional-atlas.html
